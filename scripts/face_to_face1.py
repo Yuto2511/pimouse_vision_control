c36a347f4ea21647783503a081038428cf18c8a2
Yuto2511 <efmgh2511@outlook.jp>
2021-08-17 00:29:56 +0000

    Add node and launch

--- scripts/face_to_face1.py
+++ scripts/face_to_face1.py
@@ -12,12 +12,12 @@ class FaceToFace():
 
     def get_image(self,img):
         try:
-            self.img_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
+            self.image_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError as e:
             rospy.logerr(e)
 
     def detect_face(self):
-        if self.image_org = None:
+        if self.image_org is None:
             return None
 
         return id(self.image_org), type(self.image_org), self.image_org.shape
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rospy.loginfo(fd.detect_face())
-        rete.sleep()
+        rate.sleep()
